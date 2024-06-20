7dcf54b0cae78340e3f4343242d95d13fc49a9cc
Emiliano Gonzalez <gonzalez.emiliano@fing.edu.uy>
2024-06-19 23:23:58 -0300

    remove card from landing

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BanknotesIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
 
@@ -30,12 +30,6 @@ const Home: NextPage = () => {
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BanknotesIcon className="h-8 w-8 fill-secondary" />
-              <Link href="/debug" passHref className="link">
-                <p>Tranferir Fondos</p>
-              </Link>{" "}
-            </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
