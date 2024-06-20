3804cb6a2ac5b98b3f992b4f9697b06cc13311a9
Emiliano Gonzalez <gonzalez.emiliano@fing.edu.uy>
2024-06-19 22:51:25 -0300

    First dapp template

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,62 +3,45 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BanknotesIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+import { useGlobalState } from "~~/services/store/store";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrency.price);
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-2xl mb-2">DApp de prueba</span>
+            <span className="block text-4xl font-bold">TETI</span>
           </h1>
           <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
+            <p className="my-2 font-medium">Direccion:</p>
             <Address address={connectedAddress} />
           </div>
           <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
+            Valor de ETH: <b>{nativeCurrencyPrice.toFixed(2)} USD</b>
           </p>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
+              <BanknotesIcon className="h-8 w-8 fill-secondary" />
+              <Link href="/debug" passHref className="link">
+                <p>Tranferir Fondos</p>
+              </Link>{" "}
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                  Historial de Transacciones
                 </Link>{" "}
-                tab.
               </p>
             </div>
           </div>
